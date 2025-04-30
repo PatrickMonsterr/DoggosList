217719303c06a769b5f58be74102cb91d1bba29b
Patryk Budniok <patrykbudniok@gmail.com>
2025-04-30 10:39:18 +0200

    idk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -50,7 +52,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -60,7 +61,23 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.retrofit.v290)
-    implementation(libs.converter.gson)
-    implementation(libs.coil.kt.coil.compose)
+
+
+    // Navigation
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Async Image
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
+    // Retrofit
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.retrofit)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
 }
---
+++ app/src/main/java/com/example/doggoslist/database/DogEntity.kt
@@ -0,0 +1,21 @@
+package com.verticalcoding.mystudentlist.data.local.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "dogs")
+data class DogEntity(
+    val name: String,
+
+    @ColumnInfo(defaultValue = "0")
+    var isLiked: Boolean,
+
+    @ColumnInfo(defaultValue = "null")
+    var imageUrl: String?
+
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uid: Int = 0
+}
